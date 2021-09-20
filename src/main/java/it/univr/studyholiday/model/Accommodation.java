100e9a0b2378e7306a0fddccfbfe9afd72483a4b
isabella <isabella.s.gr@gmail.com>
2021-09-20 14:46:49 +0200

    accomodation and fetch should be complete

--- src/main/java/it/univr/studyholiday/model/Accommodation.java
+++ src/main/java/it/univr/studyholiday/model/Accommodation.java
@@ -7,30 +7,28 @@ import java.time.LocalDate;
 //             dormroom,family,startdate,enddate)
 public class Accommodation {
 
-    private Student student;
-    private Holiday holiady;
+    private Reservation reservation;
     private DormRoom dormRoom;
     private Family family;
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public Accommodation(Student student, Holiday holiady,
+    public Accommodation(Reservation reservation,
                         DormRoom dormRoom, Family family,
                         LocalDate startDate, LocalDate endDate) {
-        this.student = student;
-        this.holiady = holiady;
+        this.reservation = reservation;
         this.dormRoom = dormRoom;
         this.family = family;
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
-    public Accommodation(String student, String holiadyid,
+    public Accommodation(String student, String holidayid,
                         String dormRoomnum, String family,
-                        LocalDate startDate, LocalDate endDate) {
-        this.student = Fetch.student(student);
-        this.holiady = Fetch.holiday(holiadyid);
-        this.dormRoom = Fetch.dormRoom(getHoliady().getCollege().getId(),dormRoomnum);
+                        LocalDate startDate, LocalDate endDate,
+                         String college) {
+        this.reservation = Fetch.reservation(student, holidayid);
+        this.dormRoom = Fetch.dormRoom(college,dormRoomnum);
         this.family = Fetch.family(family);
         this.startDate = startDate;
         this.endDate = endDate;
@@ -48,11 +46,8 @@ public class Accommodation {
         Delete.delete(this);
     }
 
-    public Student getStudent() {
-        return student;
-    }
-    public Holiday getHoliady() {
-        return holiady;
+    public Reservation getReservation() {
+        return reservation;
     }
     public DormRoom getDormRoom() {
         return dormRoom;
@@ -67,11 +62,10 @@ public class Accommodation {
         return endDate;
     }
 
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-    public void setHoliady(Holiday holiady) {
-        this.holiady = holiady;
+
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
     }
     public void setDormRoom(DormRoom dormRoom) {
         this.dormRoom = dormRoom;
@@ -87,7 +81,10 @@ public class Accommodation {
     }
 
 
-
-
-
+    public Student getStudent() {
+        return reservation.getStudent();
+    }
+    public Holiday getHoliday() {
+        return reservation.getHoliday();
+    }
 }
